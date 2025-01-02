5b83a9f49efc66a9e2a2add845f100aa868ccfad
p-rosit <pontus.rosqvist@gmail.com>
2025-01-02 10:54:51 +0100

    build c lib

--- build.zig
+++ build.zig
@@ -15,77 +15,54 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "con",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
+    const serialize = b.addStaticLibrary(.{
+        .name = "con-serialize",
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    b.installArtifact(lib);
+    b.installArtifact(serialize);
 
-    const exe = b.addExecutable(.{
-        .name = "con",
-        .root_source_file = b.path("src/main.zig"),
+    const fls: []const []const u8 = &.{"serialize.c"};
+    serialize.addCSourceFiles(.{
+        .root = b.path("src/serialize"),
+        .files = fls,
+    });
+    serialize.installHeader(b.path("src/serialize/serialize.h"), "serialize.h");
+
+    const deserialize = b.addStaticLibrary(.{
+        .name = "con-deserialize",
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+    b.installArtifact(deserialize);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+    const dls: []const []const u8 = &.{"deserialize.c"};
+    deserialize.addCSourceFiles(.{
+        .root = b.path("src/deserialize"),
+        .files = dls,
     });
+    deserialize.installHeader(b.path("src/deserialize/deserialize.h"), "deserialize.h");
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    // // Creates a step for unit testing. This only builds the test executable
+    // // but does not run it.
+    // const lib_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // // Similar to creating the run step earlier, this exposes a `test` step to
+    // // the `zig build --help` menu, providing a way for the user to request
+    // // running the unit tests.
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_lib_unit_tests.step);
 }
