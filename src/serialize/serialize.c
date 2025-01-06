c661face57766d78f2fbd7638598088d88d8986f
p-rosit <pontus.rosqvist@gmail.com>
2025-01-06 17:58:04 +0100

    remove asserts

--- src/serialize/serialize.c
+++ src/serialize/serialize.c
@@ -6,11 +6,8 @@ enum ConSerializeError con_serialize_context_init(
     char *out_buffer,
     int out_buffer_size
 ) {
-    assert(context != NULL);
     if (context == NULL) { return CON_SERIALIZE_NULL; }
-    assert(out_buffer != NULL);
     if (out_buffer == NULL) { return CON_SERIALIZE_NULL; }
-
     if (out_buffer_size <= 0) { return CON_SERIALIZE_BUFFER; }
 
     context->out_buffer = out_buffer;
