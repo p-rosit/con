9004736ab7e80432ba2ed99a1a2e51ce83db8967
p-rosit <pontus.rosqvist@gmail.com>
2025-01-07 08:06:53 +0100

    move tests

--- build.zig
+++ build.zig
@@ -43,7 +43,7 @@ pub fn build(b: *std.Build) void {
     );
 
     const serialize_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/serialize/serialize.zig"),
+        .root_source_file = b.path("src/serialize/test.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
