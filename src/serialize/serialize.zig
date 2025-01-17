f983bba5364442218f4f9d73b3815dacf070950c
p-rosit <pontus.rosqvist@gmail.com>
2025-01-17 08:12:13 +0100

    accept writer to Serialize

--- src/serialize/serialize.zig
+++ src/serialize/serialize.zig
@@ -4,120 +4,131 @@ const con = @cImport({
     @cInclude("serialize.h");
 });
 
-pub const Serialize = struct {
-    allocator: Allocator,
-    inner: con.ConSerialize,
+pub fn Serialize(Writer: type) type {
+    // TODO: comptime verify type passed in is writer
 
-    pub fn init(alloc: Allocator, buffer_size: usize) !Serialize {
-        var context: Serialize = .{ .inner = undefined, .allocator = alloc };
-        if (buffer_size > std.math.maxInt(c_int)) {
-            return error.Overflow;
+    return struct {
+        const Self = @This();
+
+        allocator: Allocator,
+        writer: Writer,
+        inner: con.ConSerialize,
+
+        pub fn init(w: Writer, alloc: Allocator, buffer_size: usize) !Self {
+            var context = Self{
+                .writer = w,
+                .allocator = alloc,
+                .inner = undefined,
+            };
+            if (buffer_size > std.math.maxInt(c_int)) {
+                return error.Overflow;
+            }
+
+            const err = con.con_serialize_context_init(
+                &context.inner,
+                &context.writer,
+                Self.writeCallback,
+                @ptrCast(&alloc),
+                @ptrCast(&Self.allocCallback),
+                @ptrCast(&Self.freeCallback),
+                @intCast(buffer_size),
+            );
+
+            Self.enum_to_error(err) catch |new_err| {
+                return new_err;
+            };
+            return context;
         }
 
-        const err = con.con_serialize_context_init(
-            &context.inner,
-            null,
-            Serialize.writeCallback,
-            @ptrCast(&alloc),
-            @ptrCast(&Serialize.allocCallback),
-            @ptrCast(&Serialize.freeCallback),
-            @intCast(buffer_size),
-        );
-
-        Serialize.enum_to_error(err) catch |new_err| {
-            return new_err;
-        };
-        return context;
-    }
-
-    pub fn deinit(self: Serialize) void {
-        const err = con.con_serialize_context_deinit(
-            @constCast(&self.inner),
-            @ptrCast(&self.allocator),
-            @ptrCast(&Serialize.freeCallback),
-        );
-        std.debug.assert(err == con.CON_SERIALIZE_OK);
-    }
-
-    pub fn currentPosition(self: *Serialize) usize {
-        var current_position: c_int = undefined;
-        const err = con.con_serialize_current_position(&self.inner, &current_position);
-        std.debug.assert(err == con.CON_SERIALIZE_OK);
-        std.debug.assert(current_position >= 0);
-        return @intCast(current_position);
-    }
-
-    pub fn bufferSet(self: *Serialize, buffer: []u8) !void {
-        if (buffer.len > std.math.maxInt(c_int)) {
-            return error.Overflow;
+        pub fn deinit(self: Self) void {
+            const err = con.con_serialize_context_deinit(
+                @constCast(&self.inner),
+                @ptrCast(&self.allocator),
+                @ptrCast(&Self.freeCallback),
+            );
+            std.debug.assert(err == con.CON_SERIALIZE_OK);
         }
 
-        const err = con.con_serialize_buffer_set(
-            &self.inner,
-            @ptrCast(buffer),
-            @intCast(buffer.len),
-        );
-        std.debug.assert(err == con.CON_SERIALIZE_OK);
-    }
-
-    pub fn bufferGet(self: *Serialize) []u8 {
-        var ptr: [*c]c_char = null;
-        var len: c_int = 0;
-
-        const err = con.con_serialize_buffer_get(&self.inner, @ptrCast(&ptr), &len);
-        std.debug.assert(err == con.CON_SERIALIZE_OK);
-        std.debug.assert(ptr != null);
-        std.debug.assert(len >= 0);
-
-        return @as(*[]u8, @ptrCast(@constCast(&.{ .ptr = ptr, .len = @as(usize, @intCast(len)) }))).*;
-    }
-
-    pub fn bufferClear(self: *Serialize) void {
-        const err = con.con_serialize_buffer_clear(&self.inner);
-        std.debug.assert(err == con.CON_SERIALIZE_OK);
-    }
-
-    pub fn arrayOpen(self: *Serialize) !void {
-        const err = con.con_serialize_array_open(&self.inner);
-        return Serialize.enum_to_error(err);
-    }
-
-    pub fn arrayClose(self: *Serialize) !void {
-        const err = con.con_serialize_array_close(&self.inner);
-        return Serialize.enum_to_error(err);
-    }
-
-    fn allocCallback(allocator: *anyopaque, size: usize) callconv(.C) [*c]u8 {
-        const a = @as(*const std.mem.Allocator, @ptrCast(@alignCast(allocator)));
-        const ptr = a.alignedAlloc(u8, 8, size) catch null;
-        return @ptrCast(ptr);
-    }
-
-    fn freeCallback(allocator: *anyopaque, data: [*c]u8, size: usize) callconv(.C) void {
-        std.debug.assert(null != data);
-        const a = @as(*const std.mem.Allocator, @ptrCast(@alignCast(allocator)));
-        const p = data[0..size];
-        a.free(@as([]align(8) u8, @alignCast(p)));
-    }
-
-    fn writeCallback(writer: ?*const anyopaque, data: [*c]const u8) callconv(.C) c_int {
-        std.debug.assert(null != writer);
-        std.debug.assert(null != data);
-        const w: *const std.io.AnyWriter = @alignCast(@ptrCast(writer));
-        const d = std.mem.span(data);
-        return @intCast(w.write(d) catch 0);
-    }
-
-    fn enum_to_error(err: con.ConSerializeError) !void {
-        switch (err) {
-            con.CON_SERIALIZE_OK => return,
-            con.CON_SERIALIZE_NULL => return error.Null,
-            con.CON_SERIALIZE_BUFFER => return error.Buffer,
-            con.CON_SERIALIZE_MEM => return error.Mem,
-            else => return error.Unknown,
+        pub fn currentPosition(self: *Self) usize {
+            var current_position: c_int = undefined;
+            const err = con.con_serialize_current_position(&self.inner, &current_position);
+            std.debug.assert(err == con.CON_SERIALIZE_OK);
+            std.debug.assert(current_position >= 0);
+            return @intCast(current_position);
         }
-    }
-};
+
+        pub fn bufferSet(self: *Self, buffer: []u8) !void {
+            if (buffer.len > std.math.maxInt(c_int)) {
+                return error.Overflow;
+            }
+
+            const err = con.con_serialize_buffer_set(
+                &self.inner,
+                @ptrCast(buffer),
+                @intCast(buffer.len),
+            );
+            std.debug.assert(err == con.CON_SERIALIZE_OK);
+        }
+
+        pub fn bufferGet(self: *Self) []u8 {
+            var ptr: [*c]c_char = null;
+            var len: c_int = 0;
+
+            const err = con.con_serialize_buffer_get(&self.inner, @ptrCast(&ptr), &len);
+            std.debug.assert(err == con.CON_SERIALIZE_OK);
+            std.debug.assert(ptr != null);
+            std.debug.assert(len >= 0);
+
+            return @as(*[]u8, @ptrCast(@constCast(&.{ .ptr = ptr, .len = @as(usize, @intCast(len)) }))).*;
+        }
+
+        pub fn bufferClear(self: *Self) void {
+            const err = con.con_serialize_buffer_clear(&self.inner);
+            std.debug.assert(err == con.CON_SERIALIZE_OK);
+        }
+
+        pub fn arrayOpen(self: *Self) !void {
+            const err = con.con_serialize_array_open(&self.inner);
+            return Self.enum_to_error(err);
+        }
+
+        pub fn arrayClose(self: *Self) !void {
+            const err = con.con_serialize_array_close(&self.inner);
+            return Self.enum_to_error(err);
+        }
+
+        fn allocCallback(allocator: *anyopaque, size: usize) callconv(.C) [*c]u8 {
+            const a = @as(*const std.mem.Allocator, @ptrCast(@alignCast(allocator)));
+            const ptr = a.alignedAlloc(u8, 8, size) catch null;
+            return @ptrCast(ptr);
+        }
+
+        fn freeCallback(allocator: *anyopaque, data: [*c]u8, size: usize) callconv(.C) void {
+            std.debug.assert(null != data);
+            const a = @as(*const std.mem.Allocator, @ptrCast(@alignCast(allocator)));
+            const p = data[0..size];
+            a.free(@as([]align(8) u8, @alignCast(p)));
+        }
+
+        fn writeCallback(writer: ?*const anyopaque, data: [*c]const u8) callconv(.C) c_int {
+            std.debug.assert(null != writer);
+            std.debug.assert(null != data);
+            const w: *const Writer = @alignCast(@ptrCast(writer));
+            const d = std.mem.span(data);
+            return @intCast(w.write(d) catch 0);
+        }
+
+        fn enum_to_error(err: con.ConSerializeError) !void {
+            switch (err) {
+                con.CON_SERIALIZE_OK => return,
+                con.CON_SERIALIZE_NULL => return error.Null,
+                con.CON_SERIALIZE_BUFFER => return error.Buffer,
+                con.CON_SERIALIZE_MEM => return error.Mem,
+                else => return error.Unknown,
+            }
+        }
+    };
+}
 
 const testing = std.testing;
 
