a6ddf323c2043118a421c153f6027fde37aac316
p-rosit <pontus.rosqvist@gmail.com>
2025-01-28 17:24:57 +0100

    remove deserialize step from build

--- build.zig
+++ build.zig
@@ -27,25 +27,6 @@ pub fn build(b: *std.Build) void {
         "con_serialize_writer.h",
     );
 
-    const deserialize = b.addStaticLibrary(.{
-        .name = "con-deserialize",
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-
-    b.installArtifact(deserialize);
-
-    const dls: []const []const u8 = &.{"deserialize.c"};
-    deserialize.addCSourceFiles(.{
-        .root = b.path("src/deserialize"),
-        .files = dls,
-    });
-    deserialize.installHeader(
-        b.path("src/deserialize/deserialize.h"),
-        "deserialize.h",
-    );
-
     const serialize_unit_tests = b.addTest(.{
         .root_source_file = b.path("con.zig"),
         .target = target,
@@ -57,18 +38,6 @@ pub fn build(b: *std.Build) void {
 
     const run_serialize_unit_tests = b.addRunArtifact(serialize_unit_tests);
 
-    const deserialize_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/deserialize/deserialize.zig"),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-    deserialize_unit_tests.addIncludePath(b.path("src/deserialize"));
-    deserialize_unit_tests.linkLibrary(deserialize);
-
-    const run_deserialize_unit_tests = b.addRunArtifact(deserialize_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_serialize_unit_tests.step);
-    test_step.dependOn(&run_deserialize_unit_tests.step);
 }
