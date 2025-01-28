03081c97557274e247b01a737b03a8fa5fe499ff
p-rosit <pontus.rosqvist@gmail.com>
2025-01-28 17:53:44 +0100

    document `con_serialize_init`

--- src/serialize/serialize.h
+++ src/serialize/serialize.h
@@ -27,6 +27,28 @@ struct ConSerialize {
     char state;
 };
 
+// Initializes a serialization context which can then be used to write JSON
+// with `con_serialize_array_open`, `con_serialize_number` and similar.
+//
+// Params:
+//  context:            Valid pointer to single item.
+//  writer:             Valid pointer to single item that satisfies writer
+//                      protocol specified in `writer.h`. If call succeeds
+//                      this pointer is owned by `context`.
+//  depth_buffer:       May be null if `depth_buffer_size` is 0, must otherwise
+//                      be valid pointer to as many items (or more) as specified
+//                      by `depth_buffer_size`. If call succeeds this pointer
+//                      is owned by `context`.
+//  depth_buffer_size:  must be equal to or smaller than actual length
+//                      of passed in parameter `depth_buffer`
+//
+// Error:
+//  CON_SERIALIZE_OK:       Call succeded
+//  CON_SERIALIZE_NULL:     May be returned in of the following situations
+//      1. `context` is null
+//      2. `writer` is null
+//      3. `depth_buffer` is null
+//  CON_SERIALIZE_BUFFER:   `depth_buffer_size` is negative
 enum ConSerializeError con_serialize_init(
     struct ConSerialize *context,
     void const *writer,
