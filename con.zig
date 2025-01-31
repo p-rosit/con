b83c9607966a086d8f74a3c5979663ba321ce631
p-rosit <pontus.rosqvist@gmail.com>
2025-01-31 17:47:37 +0100

    expose writer stuff

--- con.zig
+++ con.zig
@@ -1,7 +1,12 @@
 const serialize = @import("src/serialize/serialize.zig");
+const writer = @import("src/serialize/writer.zig");
 
 pub const Serialize = serialize.Serialize;
-pub const IndentJson = serialize.IndentJson;
+pub const Writer = writer.Writer;
+pub const File = writer.File;
+pub const String = writer.String;
+pub const Buffer = writer.Buffer;
+pub const Indent = writer.Indent;
 
 test {
     _ = @import("src/serialize/serialize.zig");
