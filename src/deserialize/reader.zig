0b5f003e1ab60f339223b1ba368b26688bc28abd
p-rosit <pontus.rosqvist@gmail.com>
2025-02-04 18:16:32 +0100

    test file read

--- src/deserialize/reader.zig
+++ src/deserialize/reader.zig
@@ -9,11 +9,11 @@ inline fn readData(reader: *const anyopaque, buffer: []u8) !usize {
         return error.Overflow;
     }
 
-    const result = con.con_reader_read(reader, buffer.ptr, buffer.len);
+    const result = con.con_reader_read(reader, buffer.ptr, @intCast(buffer.len));
     if (result <= 0) {
         return error.Reader;
     }
-    return result;
+    return @intCast(result);
 }
 
 pub const File = struct {
@@ -41,5 +41,38 @@ const clib = @cImport({
 
 test "file init" {
     _ = try File.init(@ptrFromInt(256));
-    return error.Writer;
+}
+
+test "file read" {
+    var file: [*c]clib.FILE = undefined;
+
+    switch (builtin.os.tag) {
+        .linux => {
+            file = clib.tmpfile();
+        },
+        .windows => {
+            @compileError("TODO: allow testing file reader, something to do with `GetTempFileNameA` and `GetTempPathA`");
+        },
+        else => {
+            std.debug.print("TODO: allow testing file writer on this os.\n", .{});
+            return;
+        },
+    }
+    defer _ = clib.fclose(file);
+
+    const written = clib.fputs("1", file);
+    try testing.expectEqual(written, 1);
+
+    const seek_err = clib.fseek(file, 0, clib.SEEK_SET);
+    try testing.expectEqual(0, seek_err);
+
+    var reader = try File.init(@as([*c]con.FILE, @ptrCast(file)));
+
+    var buffer: [1]u8 = undefined;
+    const result = try reader.read(&buffer);
+    try testing.expectEqual(1, result);
+    try testing.expectEqualStrings("1", &buffer);
+
+    const err = reader.read(&buffer);
+    try testing.expectError(error.Reader, err);
 }
