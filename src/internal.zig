fb38f6059ec3e97678835244b65c5ee7eb1ab1a1
p-rosit <pontus.rosqvist@gmail.com>
2025-02-18 07:40:17 +0100

    add new error codes to bindings

--- src/internal.zig
+++ src/internal.zig
@@ -13,6 +13,7 @@ pub fn enumToError(err: lib.ConError) !void {
         lib.CON_ERROR_OK => return,
         lib.CON_ERROR_NULL => return error.Null,
         lib.CON_ERROR_WRITER => return error.Writer,
+        lib.CON_ERROR_READER => return error.Reader,
         lib.CON_ERROR_CLOSED_TOO_MANY => return error.ClosedTooMany,
         lib.CON_ERROR_BUFFER => return error.Buffer,
         lib.CON_ERROR_TOO_DEEP => return error.TooDeep,
@@ -22,6 +23,8 @@ pub fn enumToError(err: lib.ConError) !void {
         lib.CON_ERROR_NOT_ARRAY => return error.NotArray,
         lib.CON_ERROR_NOT_DICT => return error.NotDict,
         lib.CON_ERROR_NOT_NUMBER => return error.NotNumber,
+        lib.CON_ERROR_INVALID_JSON => return error.InvalidJson,
+        lib.CON_ERROR_TRAILING_COMMA => return error.TrailingComma,
         lib.CON_ERROR_STATE_UNKNOWN => return error.StateUnknown,
         else => return error.Unknown,
     }
