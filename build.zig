ce33ffc7da5a7e645fff10f664f034377b4fa6cd
p-rosit <pontus.rosqvist@gmail.com>
2025-04-03 07:43:51 +0200

    include gci headers

--- build.zig
+++ build.zig
@@ -56,6 +56,9 @@ pub fn build(b: *std.Build) void {
         .link_libc = true,
     });
     con.addIncludePath(b.path("src"));
+    con.addIncludePath(gci.path("src"));
+    con.addIncludePath(gci.path("src/interface"));
+    con.addIncludePath(gci.path("src/implementation"));
     con.linkLibrary(serialize);
     con.linkLibrary(deserialize);
 
